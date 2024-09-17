e8661e1c920d9e7175b46efb95c2a94b44f0bd8b
luiza <luizamartins2304@gmail.com>
2024-09-17 16:46:47 -0400

    código

--- main.c
+++ main.c
@@ -5,9 +5,142 @@
 char AlfaA = {A,B,C,D,E,F,G,H,I,J};
 char AlfaB = {a,b,c,d,e,f,g,h,i,j};*/
 
+void relogio() {
+    time_t tempo_inicio, tempo_atual;
+    int segundos_decorridos = 0;
+    tempo_inicio = time(NULL);
+    while (1) {
+        tempo_atual = time(NULL);
+        segundos_decorridos = (int)(tempo_atual - tempo_inicio);
+        printf("\rTempo decorrido: %d segundos", segundos_decorridos);
+        fflush(stdout);
+        sleep(1);
+    }
+}
+
+void ajuda() {
+
+    printf("Regras do jogo:\n");
+}
 
 void menu() {
+    printf(
+        "\n   MENU   \n"
+        "A - AJUDA\n"
+        "D - DICA\n"
+        "C - CONFIGURACAO\n"
+        "I - INICIAR\n"
+        "P - PAUSAR\n"
+        "T - CONTINUAR\n"
+        "J - JOGADA\n"
+        "S - SAIR\n"
+        "Escolha uma opcao: \n"
+    );
+}
 
+
+void opcao(char op) {
+    switch (op) {
+        case 'A':
+        case 'a':
+            ajuda();
+        break;
+        case 'D':
+        case 'd':
+            dica();
+        break;
+        case 'C':
+        case 'c':
+            configuracao();
+        break;
+        case 'I':
+        case 'i':
+            iniciar();
+        break;
+        case 'P':
+        case 'p':
+            pausa();
+        break;
+        case 'T':
+        case 't':
+            continuar();
+        break;
+        case 'J':
+        case 'j':
+            jogada();
+        break;
+        case 'S':
+        case 's':
+            sair();
+        break;
+        default:
+            printf("Opcao invalida! Tente novamente.\n");
+    }
+}
+
+
+//funcao que da uma dica para o jogador
+void dica() {
+    count_dicas++;
+    switch (count_dicas) {
+        case 1:
+            printf("Dica: comece pelas bordas, pode ser mais seguro\n\n.");
+            break;
+        case 2:
+            printf("Dica: Use os numeros, eles mostram quantas minas tem ao redor.\n\n");
+            break;
+        case 3:
+            printf("Dica: Cuidado com os numeros altos, como (5) indica muitas minas por perto.\n\n");
+            break;
+        case 4:
+            printf("Dica: Encontrou um (1)? Se você já marcou uma mina ao lado, as outras casas ao redor são seguras para abrir. Isso pode limpar uma grande área de uma vez.\n\n");
+            break;
+        case 5:
+            printf("Dica: ");
+            break;
+        case 6:
+            printf("Dica 6");
+            break;
+        case 7:
+            printf("Dica 7");
+            break;
+        case 8:
+            printf("Dica 8");
+            break;
+            count_dicas = 0;
+    }
+}
+
+void configuracao() {
+    int nivel;
+    printf("Selecione o nivel de dificuldade(1, 2 ou 3)");
+    scanf("%d", &nivel);
+    printf("Nivel %d selecionado", nivel);
+}
+
+void iniciar() {
+    printf("Jogo iniciado\n");
+    printf("Tabuleiro montado e relogio iniciado!");
+}
+
+void pausa() {
+    printf("Jogo pausado");
+}
+
+void continuar() {
+    printf("Retomando o jogo");
+}
+
+void jogada() {
+    int linha, coluna;
+    printf("Informe a linha e a coluna para revelar uma celula: ");
+    scanf("%d %d", &linha, &coluna);
+    printf("Celula (%d, %d) revelada.\n", linha, coluna);
+}
+
+void sair () {
+    printf("Jogo finalizado!");
+    exit(0);
 }
 
 void geraTabIni(int tab[10][10]){
@@ -39,20 +172,27 @@ void imprime(int mat[10][10]){
 
         printf("\n");
     }
-
 }
 
 int main() {
-    printf("Campo Minado iniciado\n");
     int mat[10][10];
+    int tabInic[10][10];
+    char op;
+
+    printf("Campo Minado iniciado\n");
 
     geraCampo(mat);
-    int tabInic[10][10];
     geraTabIni(tabInic);
     imprime(tabInic);
 
+    while(1) {
+        menu();
+        scanf("%c", &op);
+        getchar();
+        opcao(op);
+    }
+
+    return 0;
 
 }
-//
-// Created by luiza on 16/09/2024.
-//
+
